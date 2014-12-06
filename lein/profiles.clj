9581efc743c0b0600d434162003179ce45694275
Henry Garner <henry@henrygarner.com>
2014-12-06 12:41:48 +0000

    Set up magit key binding

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user
- {:dependencies [[slamhound "1.5.5"]]
+ {:dependencies [[slamhound "1.5.5"]
+                 [com.cemerick/pomegranate "0.3.0"]]
   :plugins [[cider/cider-nrepl "0.8.2-SNAPSHOT"]
             [refactor-nrepl "0.1.0"]
             [mvxcvi/whidbey "0.4.1"]
